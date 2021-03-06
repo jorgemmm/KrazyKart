0ceb8ee452e959391cdd6fab3533f2d26b6e068b
Jordi <vplasin@gmail.com>
2021-03-06 02:19:35 +0000

    rotate amd move

--- Source/KrazyKart/GoKart.cpp
+++ Source/KrazyKart/GoKart.cpp
@@ -25,9 +25,48 @@ void AGoKart::BeginPlay()
 void AGoKart::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	//Velmax= |1| * MaxDrivingForce * |1|= MaxDrivingForce
+	FVector Force = GetActorForwardVector() * MaxDrivingForce * Acelerador;//Throttle;
+	
+	FVector Acceleration = Force / Mass;
+
+
+	Velocity = Velocity + Acceleration * DeltaTime;
+
+
+	Rotate(DeltaTime);
+
+	Move(DeltaTime);
+	//AddActorWorldOffset(Translation, true);
+}
+void AGoKart::Rotate(float DeltaTime)
+{
+	float RotatationAngle = MaxDegreePerSecond * DeltaTime* Volante;//SteeringThrow;
+
+	UE_LOG(LogTemp, Warning, TEXT("Giro en Tick: %f "), Volante);
+	UE_LOG(LogTemp, Warning, TEXT("RotatationAngle: %f "), RotatationAngle);
+	
+	FQuat RotationDelta(GetActorUpVector(),FMath::DegreesToRadians( RotatationAngle ));
+
+	//Update Velocity
+	Velocity = RotationDelta.RotateVector(Velocity);
+
+	AddActorWorldRotation(RotationDelta);
+}
+/**UpdateLocationFromVelocity*/
+void AGoKart::Move(float DeltaTime)
+{
 	FVector Translation = Velocity * 100 * DeltaTime;
 
-	AddActorWorldOffset(Translation);
+	FHitResult Hit;
+	AddActorWorldOffset(Translation, true, &Hit);
+	if (Hit.IsValidBlockingHit())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Algo ha bloqueado al al kart"));
+		Velocity = FVector::ZeroVector;
+		UE_LOG(LogTemp, Warning, TEXT("Velocity: %f "), Velocity.Size());
+	}
 }
 
 // Called to bind functionality to input
@@ -35,19 +74,39 @@ void AGoKart::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	PlayerInputComponent->BindAxis("MoveForward", this, &AGoKart::MoveForward);
-	PlayerInputComponent->BindAxis("MoveRight", this, &AGoKart::MoveRight);
-
+	//PlayerInputComponent->BindAxis("MoveForward", this, &AGoKart::MoveForward);
+	//PlayerInputComponent->BindAxis("MoveRight", this, &AGoKart::MoveRight);
+	PlayerInputComponent->BindAxis("MoveY", this, &AGoKart::MoveY);
+	PlayerInputComponent->BindAxis("MoveX", this, &AGoKart::MoveX);
 }
 
 
 void AGoKart::MoveForward(float Value)
 {
-	//
-	Velocity = GetActorForwardVector() * 20 * Value;
+	 Throttle = Value;
+	 //Velmax= 1 * 20 * |1| 
+	//Velocity = GetActorForwardVector() * 20 * Value;
 }
 
 void AGoKart::MoveRight(float Value)
 {
+	SteeringThrow = Value;
+}
 
+
+
+void AGoKart::MoveY(float Value)
+{
+
+	Acelerador = Value;
+
+	UE_LOG(LogTemp, Warning, TEXT("Acelerador en Input: %f "), Throttle);
+}
+
+void AGoKart::MoveX(float Value)
+{
+	
+	Volante = Value;
+
+	UE_LOG(LogTemp, Warning, TEXT("Giro en Input: %f "), Volante);
 }
--- Source/KrazyKart/GoKart.h
+++ Source/KrazyKart/GoKart.h
@@ -25,21 +25,54 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	
+
+	
+
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 private:
 
-	/**Acelerador Translation*/
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SettingKart", meta = (AllowPrivateAccess = "true"))
-		float Throttle =100.f;
-	
-	void MoveForward(float Value);
+	void Rotate(float DeltaTime);
 
-	void MoveRight(float Value);
+	void Move(float DeltaTime);
+
+	/**The mass of the car in (kg)*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SettingKart", meta = (AllowPrivateAccess = "true"))
+		float Mass = 1000;
+	
+	
+	/**Acelerador Translation Deprec*/		
+	float Throttle;
+	/**Vplante Giro Deprec*/
+	float SteeringThrow;
+
+	/**Acelerador Translation*/
+	float Volante, Acelerador;
+
+	//The force applied to th car when the Throttle is fully down in (N)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SettingKart", meta = (AllowPrivateAccess = "true"))
+		float MaxDrivingForce = 10000.0f;
+
+	//The number of degrees rotated per second at full controll thtow fully in (degrees/s)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SettingKart", meta = (AllowPrivateAccess = "true"))
+		float MaxDegreePerSecond = 90.0f;
+
+	
+	void MoveForward(float Value); //Deprec
+
+	void MoveRight(float Value);  //Deprec
+
+	void MoveX(float Value);
+
+	void MoveY(float Value);
+
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "SettingKart", meta = (AllowPrivateAccess = "true"))
-		FVector Velocity = FVector();;
+		FVector Velocity = FVector(0.f,0.f,0.f);
+
+
 
 
 
